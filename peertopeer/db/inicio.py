c41d11a651e182e18fddb124fdea3089884bbb33
Carlos Arias <carlosariassnadoval34@gmail.com>
2024-10-01 15:56:15 -0600

    eliminacion de archivos BD

--- peertopeer/db/inicio.py
+++
@@ -1,8 +0,0 @@
-from peertopeer.db.conexion import Conexion
-
-cx=Conexion('EducamEsta',1,'DB.sql')
-#cx=Conexion()
-
-#print(cx.detectar_Instruccion('use irrepetible'))
-
-#print(cx.detectar_Instruccion('create schema irrepetible'))
